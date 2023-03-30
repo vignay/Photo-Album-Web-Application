249385ff45a03f70cb521369435dcd3b6ba81153
Adithya K Krishna <adithya.krishnakumar@gmail.com>
2023-03-30 12:20:16 -0400

    added frontend and lambdas

---
+++ FrontEnd/index.js
@@ -0,0 +1,101 @@
+// Variables for accessing the API Gateway endpoints
+const API_BASE_URL = 'https://lg47up03v6.execute-api.us-east-1.amazonaws.com/Photo_Stage1';
+const SEARCH_ENDPOINT = '/search';
+const UPLOAD_ENDPOINT = '/upload/photo-storage-b2/';
+
+// Function to handle text search
+function textSearch() {
+  // Get the search query from the input field
+  const searchQuery = document.getElementById('search_query').value;
+
+  // Make an HTTP GET request to the search endpoint with the search query as a parameter
+  fetch(API_BASE_URL + SEARCH_ENDPOINT + '?q=' + searchQuery)
+    .then(response => response.json())
+    .then(data => {
+      if (data.length === 0) {
+        var photosDiv = document.getElementById("photos_search_results");
+        photosDiv.innerHTML = "";
+
+        // Create a new HTML element to display the message
+        const message = document.createElement('p');
+        message.textContent = 'No photos found';
+        photosDiv.appendChild(message);
+    } else {
+        var photosDiv = document.getElementById("photos_search_results");
+        photosDiv.innerHTML = "";
+        
+        for (let i = 0; i < data.length; i++) {
+            const photo = data[i];
+            const bucket = photo.bucket;
+            const objectKey = photo.objectKey;
+        
+            const photoHtml = '<figure style="display:inline-block; margin:10px; height: 100px; width:calc(100%/3 - 20px)">' +
+                                '<img src="' + `https://${bucket}.s3.amazonaws.com/${objectKey}` + '" style="width:100%; max-height:100px">' +
+                                '<figcaption style="text-align:center">' + objectKey + '</figcaption>' +
+                            '</figure>';
+            photosDiv.innerHTML += photoHtml;
+        }
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+// Function to perform a voice search
+function voiceSearch() {
+  const $micIcon = $('#mic_search');
+  const recognition = new webkitSpeechRecognition();
+
+  recognition.onstart = function() {
+    $micIcon.text('mic_off');
+  };
+
+  recognition.onresult = function(event) {
+    const query = event.results[0][0].transcript;
+    $('#search_query').val(query);
+    textSearch();
+  };
+
+  recognition.onerror = function(event) {
+    console.error(event.error);
+  };
+
+  recognition.onend = function() {
+    $micIcon.text('mic');
+  };
+
+  recognition.start();
+}
+
+// Function to handle photo upload
+function uploadPhoto() {
+  // Get the uploaded file and custom labels from the input fields
+  const uploadedFile = document.getElementById('uploaded_file').files[0];
+  const customLabels = document.getElementById('custom_labels').value;
+
+  // Create a new FormData object and append the uploaded file and custom labels to it
+  const formData = new FormData();
+  formData.append('file', uploadedFile);
+  formData.append('labels', customLabels);
+
+  const fileInput = document.getElementById('uploaded_file');
+  const fileName = fileInput.value.split('\\').pop(); 
+  const fileType = fileName.split('.').pop();
+  console.log(fileName); 
+  console.log('file type',fileType);
+  console.log('cusLab', customLabels);
+
+
+  // Make an HTTP PUT request to the upload endpoint with the file and custom labels as parameters
+  fetch(API_BASE_URL + UPLOAD_ENDPOINT + fileName, {
+    method: 'PUT',
+    headers: {
+      "x-amz-meta-customLabels": customLabels.value,
+      "Content-Type": `image/${fileType}`,
+      "x-api-key": "EJj1AAyQwB6kTf5PN6N079TIoWzxGbeJ9nQutCVY"
+    },
+    body: uploadedFile
+  })
+    .then(response => response)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+}
